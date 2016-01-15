67de6d8f5bfbd64cc82f6f178ddd24e1273b2c9d
Adam Gashlin <agashlin@gmail.com>
2016-01-15 13:04:12 -0500

    reorder packages, rename README

--- package.sh
+++ package.sh
@@ -1,3 +1,23 @@
 ZIPNAME=${1:?}
 
-zip "ww2ogg$ZIPNAME.zip" src/Bit_stream.h src/codebook.cpp src/codebook.h COPYING src/crc.c src/crc.h src/errors.h CHANGELOG Makefile Makefile.common Makefile.mingw packed_codebooks.bin packed_codebooks_aoTuV_603.bin src/ww2ogg.cpp ww2ogg.exe src/wwriff.cpp src/wwriff.h package.sh notes.md README
+zip "ww2ogg$ZIPNAME.zip" \
+  src/Bit_stream.h \
+  src/codebook.cpp \
+  src/codebook.h \
+  src/crc.c \
+  src/crc.h \
+  src/errors.h \
+  src/ww2ogg.cpp \
+  src/wwriff.cpp \
+  src/wwriff.h \
+  CHANGELOG \
+  COPYING \
+  Makefile \
+  Makefile.common \
+  Makefile.mingw \
+  notes.md \
+  packed_codebooks.bin \
+  packed_codebooks_aoTuV_603.bin \
+  package.sh \
+  README \
+  ww2ogg.exe
